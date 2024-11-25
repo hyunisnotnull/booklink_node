add1b31e127149ca904c0382b3911eb55608895f
wkdwlsdjs <wkdwlsdjs@gmail.com>
2024-11-25 17:39:36 +0900

    add search library

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const bookService = require('../lib/service/bookService');
 
-// /book
+// 책 상세검색
 router.get('/detail/:bookID', (req, res) => {  
     console.log('/book/detail/:bookID');
     bookService.detail(req, res);
@@ -12,6 +12,11 @@ router.get('/detail/:bookID', (req, res) => {
 router.get('/search_book', (req, res) => {  
     console.log('/book/search_book');
     bookService.search_book(req, res);
+});
+
+router.get('/search_library', (req, res) => {  
+    console.log('/book/search_library');
+    bookService.search_library(req, res);
 });
 
 module.exports = router;
