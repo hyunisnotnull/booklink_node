3e5ca2cd1822ca18c1af46a787072c6a0c47afed
hyun <bongtacker@naver.com>
2024-11-26 17:40:37 +0900

    edit events

--- routes/eventRouter.js
+++ routes/eventRouter.js
@@ -17,25 +17,25 @@ router.post('/register_event_confirm', uploads.UPLOAD_EVENT_IMAGES_MIDDLEWARE(),
 });
 
 // 이벤트 수정 폼
-router.get('/modify_event_form/:id', (req, res) => {
+router.get('/modify_event_form/:eventId', (req, res) => {
     console.log('/event/modify_event_form');
     eventService.modifyEventForm(req, res);
 });
 
 // 이벤트 수정 처리
-router.put('/modify_event_confirm/:id', uploads.UPLOAD_EVENT_IMAGES_MIDDLEWARE(), (req, res) => {
+router.put('/modify_event_confirm/:eventId', uploads.UPLOAD_EVENT_IMAGES_MIDDLEWARE(), (req, res) => {
     console.log('/event/modify_event_confirm');
     eventService.modifyEventConfirm(req, res);
 });
 
 // 이벤트 상태 변경 처리
-router.post('/event_status/:id', (req, res) => {
+router.put('/event_status/:eventId', (req, res) => {
     console.log('/event/event_status');
     eventService.eventStatus(req, res);
 });
 
 // 이벤트 삭제 처리
-router.post('/delete_event_confirm', (req, res) => {
+router.delete('/delete_event_confirm/:eventId', (req, res) => {
     console.log('/event/delete_event_confirm');
     eventService.deleteEventConfirm(req, res);
 });
