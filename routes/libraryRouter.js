4b1a68aacce6577b9341e120642653385b394093
hyun <bongtacker@naver.com>
2024-11-28 17:31:32 +0900

    add loanLibrary

--- routes/libraryRouter.js
+++ routes/libraryRouter.js
@@ -4,8 +4,14 @@ const libraryService = require('../lib/service/libraryService');
 
 // library JPA
 router.post('/JPA', (req, res) => {  
-    console.log('/library');
+    console.log('/library/JPA');
     libraryService.updateLibraryJPA(req, res);
+});
+
+// 도서관 대출, 소장 여부 롸우톼
+router.get('/loanAvailable', (req, res) => {  
+    console.log('/library/loanAvailable');
+    libraryService.getLoanAvailable(req, res);
 });
 
 module.exports = router;
