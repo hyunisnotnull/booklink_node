da70326e817fca717b3727347bf656c932ead769
wkdwlsdjs <wkdwlsdjs@gmail.com>
2024-12-12 11:13:10 +0900

    change log

--- routes/statRouter.js
+++ routes/statRouter.js
@@ -4,19 +4,19 @@ const statService = require('../lib/service/statService');
 
 // 읽은 책 가지고 오기
 router.get('/book_read', (req, res) => {  
-    console.log('/stat/book_read');
+    logger.info('/stat/book_read');
     statService.book_read(req, res);
 });
 
 // 도서 찜 순위 가지고 오기
 router.get('/book_rank', (req, res) => {  
-    console.log('/stat/book_rank');
+    logger.info('/stat/book_rank');
     statService.book_rank(req, res);
 });
 
 // 도서관 찜 순위 가지고 오기
 router.get('/library_rank', (req, res) => {  
-    console.log('/stat/library_rank');
+    logger.info('/stat/library_rank');
     statService.library_rank(req, res);
 });
 
